ab605e96eedb752b25e6bf9c2003b33f24f39d57
kmmaniez <dzlabsid@gmail.com>
2023-09-05 09:59:45 +0700

    update dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -29,9 +29,27 @@
                         </div>
                     </div>
                 </div>
+                
+                <!-- Total barang retur -->
+                <div class="col-xl-2 col-md-6 mb-4">
+                    <div class="card border-bottom-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Total Barang Retur</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-list fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
                 <!-- Total barang masuk -->
-                <div class="col-xl-3 col-md-6 mb-4">
+                <div class="col-xl-2 col-md-6 mb-4">
                     <div class="card border-bottom-success shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
@@ -49,7 +67,7 @@
                 </div>
                 
                 <!-- Total barang keluar -->
-                <div class="col-xl-3 col-md-6 mb-4">
+                <div class="col-xl-2 col-md-6 mb-4">
                     <div class="card border-bottom-success shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
