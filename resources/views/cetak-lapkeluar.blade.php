a60aa689b8f104c4d2dddecf72bfd34c79f1e838
kmmaniez <dzlabsid@gmail.com>
2023-10-04 20:07:51 +0700

    update views cetak

--- resources/views/cetak-lapkeluar.blade.php
+++ resources/views/cetak-lapkeluar.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +8,25 @@
     <title></title>
 </head>
 <style>
-    table{
+    table {
         width: 100%;
-        /* border: 2px solid #000; */ 
+        /* border: 2px solid #000; */
         /* border: 1px inset #000; */
     }
+    #header{
+        text-align: center;
+        width: 100%;
+        height: max-content;
+    }
 </style>
+
 <body>
-    <h1>Laporan Masuk</h1>
+    <div id="header">
+        <h1 style="color: red;">CV. PRASETYA</h1>
+        <p>Jl. Slompretan No. 57 Surabaya</p>
+        <p>Telepon (031) 3557777 Hunting Fax. (031) 3557077</p>
+    </div>
+    <hr>
     <table border="1">
         <thead>
             <tr>
@@ -29,17 +41,18 @@
         </thead>
         <tbody>
             @foreach ($data as $laporan)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $laporan->nama_bgudang }}</td>
-                <td>{{ $laporan->masterproduk->nama_produk }}</td>
-                <td>{{ $laporan->warna }}</td>
-                <td>{{ $laporan->ukuran }}</td>
-                <td>{{ $laporan->stok }}</td>
-                <td>{{ $laporan->tgl_keluar }}</td>
-            </tr>
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $laporan->nama_bgudang }}</td>
+                    <td>{{ $laporan->masterproduk->nama_produk }}</td>
+                    <td>{{ $laporan->warna }}</td>
+                    <td>{{ $laporan->ukuran }}</td>
+                    <td>{{ $laporan->stok }}</td>
+                    <td>{{ $laporan->tgl_keluar }}</td>
+                </tr>
             @endforeach
         </tbody>
     </table>
 </body>
+
 </html>
